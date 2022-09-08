b994cdcddf9b8e1f237dcc55a70262852956feb6
Rameshmythri <100982758+Rameshmythri@users.noreply.github.com>
2022-09-08 13:00:32 +0530

    welcome Sai

---
+++ src/com/nt/sai/WelcomeSai.java
@@ -0,0 +1,15 @@
+package com.nt.sai;
+
+public class WelcomeSai {
+
+	public WelcomeSai() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Sai! How are You \nWelcome to the Online Cloud Technology");
+
+	}
+
+}
